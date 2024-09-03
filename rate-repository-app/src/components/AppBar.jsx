31c395189e1a24ecc74b1631f02eaa9839b4fa06
Thanatos <Thanatosh@gmail.com>
2024-09-03 14:58:41 +0300

    Part 10.6-7 Full Commit

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,24 +1,41 @@
-import { View, StyleSheet, Pressable } from 'react-native';
-import Constants from 'expo-constants';
-import theme from '../theme';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Text from './Text';
+import { Link } from 'react-router-native';
+import theme from '../theme';
 
 const styles = StyleSheet.create({
   container: {
-    height: 70,
-    backgroundColor: theme.colors.barBackground,
-    paddingTop: Constants.statusBarHeight,
-    paddingHorizontal: 15,
+    height: 80,
+    backgroundColor: theme.colors.textPrimary,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingHorizontal: 10,
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginHorizontal: 10,
+  },
+  link: {
+    textDecorationLine: 'none',
   },
 });
 
 const AppBar = () => {
   return (
-    <Pressable onPress={() => { /* ... */ }}>
-      <View style={styles.container}>
-        <Text style={{ color: 'white' }} fontWeight="bold" fontSize="subheading">Repositories</Text>
-      </View>
-    </Pressable>
+    <View style={styles.container}>
+      <ScrollView horizontal>
+        <Link to="/" style={styles.link}>
+          <Text style={styles.text}>Repositories</Text>
+        </Link>
+        <Link to="/signin" style={styles.link}>
+          <Text style={styles.text}>Sign In</Text>
+        </Link>
+      </ScrollView>
+    </View>
   );
 };
 
--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -1,7 +1,9 @@
 import { StyleSheet, View } from 'react-native';
 import RepositoryList from './RepositoryList';
+import SignIn from './SignIn';
 import AppBar from './AppBar';
 import theme from '../theme';
+import { Route, Routes } from 'react-router-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,7 +17,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
     </View>
   );
 };
