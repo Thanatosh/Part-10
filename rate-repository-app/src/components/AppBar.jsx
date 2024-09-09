98781a8702cc7ddee04dcd3a48f8500fb15536dc
Thanatos <Thanatosh@gmail.com>
2024-09-09 09:17:58 +0300

    Part 10.16 Full Commit

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,7 +1,10 @@
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView, Pressable } from 'react-native';
+import { Link, useNavigate } from 'react-router-native';
+import { GET_AUTHENTICATED_USER } from '../graphql/queries';
+import { useApolloClient, useQuery } from '@apollo/client';
 import Text from './Text';
-import { Link } from 'react-router-native';
 import theme from '../theme';
+import useAuthStorage from '../hooks/useAuthStorage';
 
 const styles = StyleSheet.create({
   container: {
@@ -25,15 +28,32 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const { data } = useQuery(GET_AUTHENTICATED_USER);
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    await authStorage.removeAccessToken();
+    await apolloClient.resetStore();
+    navigate('/signin');
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView horizontal>
         <Link to="/" style={styles.link}>
           <Text style={styles.text}>Repositories</Text>
         </Link>
-        <Link to="/signin" style={styles.link}>
-          <Text style={styles.text}>Sign In</Text>
+        {data?.me ? (
+        <Pressable onPress={handleSignOut} style={styles.link}>
+          <Text style={styles.text}>Sign out</Text>
+        </Pressable>
+      ) : (
+        <Link to="/signin" component={Pressable} style={styles.link}>
+          <Text style={styles.text}>Sign in</Text>
         </Link>
+      )}
       </ScrollView>
     </View>
   );
